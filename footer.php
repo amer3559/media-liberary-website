1a12ed1a79ec83a88cd1eaa8a4619a8f22d4a1ac
Amer_Morsy <amer.morsy2015@gmail.com>
2020-07-04 19:25:04 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<p>&copy;<?php echo date("Y"); ?> Personal Media Library | Coded By <a href="http://amermorsy.ezyro.com/?i=2">Amer</a></p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
